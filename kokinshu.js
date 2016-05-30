9888210e8eb130aaf6aa335c28fe35e2b158be30
chasholden <chasholden0@gmail.com>
2016-05-29 17:35:33 -0700

    added responsive columns to controls

--- kokinshu.js
+++ kokinshu.js
@@ -1,4 +1,4 @@
-//---------------------filter selections--------------------
+//-------------------------------filter selections------------------------------
 
 $(document).ready(function() {
   $("select").on("change", runFilters)
@@ -8,13 +8,14 @@ $(document).ready(function() {
     var filterVal2 = $("select#filter2").val();
     var filterVal3 = $("select#filter3").val();
     var filterVal4 = $("select#filter4").val();
+    var filterVal5 = $("select#filter5").val();
     var filterSelector = "";
     var filter2Selector = "";
     var filter3Selector = "";
     var filter4Selector = "";
+    var filter5Selector = "";
 
-    if (filterVal == "all" && filterVal2 == "all" && filterVal3 === null && filterVal4 == "all") {
-      //show all items 
+    if (filterVal == "all" && filterVal2 == "all" && filterVal3 === null && filterVal4 == "all" && filterVal5 == "all") {
       $("li").fadeIn("fast");
     } else {
       if (filterVal != "all") {
@@ -32,13 +33,17 @@ $(document).ready(function() {
       if (filterVal4 != "all") {
         filter4Selector = "." + filterVal4;
       }
+      
+      if (filterVal5 != "all") {
+        filter5Selector = "." + filterVal5;
+      }
 
       $("li").hide();
-      $(filterSelector + filter2Selector + filter3Selector + filter4Selector).fadeIn("fast");
+      $(filterSelector + filter2Selector + filter3Selector + filter4Selector + filter5Selector).fadeIn("fast");
     }
   }
 
-//-------------------search functions------------------------
+//------------------------------search functions--------------------------------
   
   
   $("#main-search").keyup(function() {
@@ -71,14 +76,14 @@ $(document).ready(function() {
     }
   });
 
+  //-------------------------------click events-------------------------------------
   
   
-  //------------------click events-------------------------------------
   $("#slide-toggle").click(function() {
     var expand = $(this);
     var content = expand.prev();
     $("#slide-toggle").toggleClass("open");
-    $("#poem-list").toggleClass("controls-open");
+    $("h1").toggleClass("controls-open");
     $("#controls").slideToggle(50, function() {
       expand.text(function() {
         return content.is(":visible") ? "Hide controls" : "Show controls";
